185c39704424240033d1092795305644c47cf143
dziper <dziper@me.com>
2025-05-25 18:11:59 -0700

    initial commit

---
+++ conc_testing/src/main.rs
@@ -0,0 +1,20 @@
+async fn say_world() {
+    println!("world");
+}
+
+#[tokio::main]
+async fn main() {
+    // Calling `say_world()` does not execute the body of `say_world()`.
+    let op = say_world();
+
+    // This println! comes first
+    println!("hello");
+
+    // Calling `.await` on `op` starts executing `say_world`.
+    op.await;
+}
+
+#[tokio::test]
+async fn test_something_async() {
+    assert!(true);
+}
