08401f820dbcda6a6ab921c4c34b826a0dd30f64
dziper <dziper@me.com>
2025-06-12 11:49:48 -0700

    big problem... we need to duplicate functions

--- example_proj/src/funcs.rs
+++ example_proj/src/funcs.rs
@@ -1,4 +1,7 @@
 use tokitest::{testable, label};
+use std::sync::Arc;
+use tokio::{sync::RwLock};
+use tokio::time::{sleep, Duration};
 
 #[testable]
 pub async fn my_async_func() {
@@ -8,3 +11,13 @@ pub async fn my_async_func() {
     println!("Thing 2");
     label!("label 2");
 }
+
+#[testable]
+pub async fn nested_shared_write(offset: i32, data: Arc<RwLock<Vec<i32>>>) {
+	for i in 0..10 {
+		data.write().await.push(offset + i);
+		label!("loop label");
+		sleep(Duration::from_millis(10)).await;
+	}
+}
+
--- example_proj/src/main.rs
+++ example_proj/src/main.rs
@@ -1,10 +1,20 @@
 use tokitest::{call};
 use example_proj::funcs::my_async_func;
 
+#[cfg(feature = "tokitest")]
+fn main() {}
+
+#[cfg(not(feature = "tokitest"))]
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
 
-    call!(my_async_func()).await;
+    // call!(my_async_func()).await;
+
+    thingy().await;
 }
 
+
+async fn thingy() {
+    my_async_func().await;
+}
